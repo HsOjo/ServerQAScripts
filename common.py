5e443ae0f0828fc1a3f73a2d1faf07c360b53f26
HsOjo <1134031392@qq.com>
2020-01-03 04:41:58 +0800

    Fix encoding.

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def str_compare_head_tail(text, head, tail=None):
@@ -8,5 +9,6 @@ def str_compare_head_tail(text, head, tail=None):
 
 
 def sub_exec(args, input=None, timeout=None, **kwargs):
+    kwargs.setdefault('encoding', sys.getdefaultencoding())
     proc = subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, **kwargs)
     return proc.communicate(input, timeout)
